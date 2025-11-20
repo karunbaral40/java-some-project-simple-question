458fd0aeb5d5b55e6b1483fd403655537c00f9f8
karunbaral40 <karunbaral40@gmail.com>
2025-11-20 14:38:24 +0545

    add some code

---
+++ loops/PattrenPrinting/src/firstPattern.java
@@ -0,0 +1,24 @@
+//    Print a simple pattern of stars (*) for 5 rows:
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
+import java.util.Scanner;
+public class firstPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of i");
+        int n=sc.nextInt();
+        System.out.println("using for loop");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <i ; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+
+
+    }
+
+}
