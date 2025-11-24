73fd4f85c3065ce8a1ebbceff02315dfa3c8c7a0
karunbaral40 <karunbaral40@gmail.com>
2025-11-24 21:24:13 +0545

    add another one

---
+++ method in java/src/addFirstNeturalNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class addFirstNeturalNumber {
+    static int netural(int a){
+     int sum=0;
+        for (int i = 1; i <=a ; i++) {
+            sum=sum+i;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the natural number ");
+        int num= sc.nextInt();
+        int sum=netural(num);
+        System.out.println("the sum is"+sum);
+    }
+}
