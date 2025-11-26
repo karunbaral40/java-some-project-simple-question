614597f8f796ce000c7ae3957090c555c7845a0c
karunbaral40 <karunbaral40@gmail.com>
2025-11-26 20:59:43 +0545

    new same

--- method in java/src/SumUsingVariableArgumentOneValueIsContent.java
+++ method in java/src/SumUsingVariableArgumentOneValueIsContent.java
@@ -1,2 +1,17 @@
 public class SumUsingVariableArgumentOneValueIsContent {
+    static int sum(int x ,int... a){
+        int result=x;
+        for(int value:a){
+            result+=value;
+        }
+        return result;
+
+    }
+    public static void main(String[] args) {
+        System.out.println("the sum of 2,3,4,5 is :" + sum(2,3 , 4 , 5));
+        System.out.println("the sum of 2,3,4,5,6 is :" + sum(2,3 , 4 , 5,6));
+        System.out.println("the sum of 2,3,4,5,6,7 is :" + sum(2,3 , 4 , 5 , 6 , 7));
+        System.out.println("the sum of 2,3,4,5,6,7,8 is :" + sum(2,3 , 4 , 5 , 6 , 7 ,8));
+
+    }
 }
