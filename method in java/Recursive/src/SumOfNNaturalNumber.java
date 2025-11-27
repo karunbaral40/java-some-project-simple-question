402826c272c07c08cc4ed88ac7ca09396c9e178d
karunbaral40 <karunbaral40@gmail.com>
2025-11-27 13:38:31 +0545

    recursion

--- method in java/Recursive/src/SumOfNNaturalNumber.java
+++ method in java/Recursive/src/SumOfNNaturalNumber.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
 public class SumOfNNaturalNumber {
-}
+    static int sum(int num) {
+        int sum = 0;
+        if (num == 1) {
+            return 1;
+        } else {
+            for (int i = 1; i <= num; i++) {
+                sum = sum + i;
+                System.out.println("sum is :" + sum);
+
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter an number:");
+        int n= sc.nextInt();
+        sum(n);
+
+
+        }
+
+    }
+
