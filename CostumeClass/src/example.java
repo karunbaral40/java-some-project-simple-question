26a60cc30dd986906409f4e2239328a9deb0505a
karunbaral40 <karunbaral40@gmail.com>
2025-11-28 20:00:26 +0545

    example of costume class

---
+++ CostumeClass/src/example.java
@@ -0,0 +1,13 @@
+class student{
+    int id;
+    String name;
+}
+public class example {
+    public static void main(String[] args) {
+        student detils=new student();
+        detils.id=10;
+        detils.name="karun";
+        System.out.println(detils.id);
+        System.out.println(detils.name);
+    }
+}
--- method in java/Recursive/src/PatternPrinting.java
+++ method in java/Recursive/src/PatternPrinting.java
@@ -1,5 +1,5 @@
 public class PatternPrinting {
-    
+
     public static void main(String[] args) {
 
     }
