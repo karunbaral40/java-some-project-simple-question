5c27863d0492615a0c3369a2687426ac4ec0ed49
karunbaral40 <karunbaral40@gmail.com>
2025-12-05 20:00:38 +0545

    pattrun printing

---
+++ loops/PattrenPrinting/src/second.java
@@ -0,0 +1,24 @@
+//Pattren :-
+//        *****
+//         ****
+//          ***
+//           **
+//            *
+public class second {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 5; i >= 1; i--) {
+
+            for (int s = 0; s < n - i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
