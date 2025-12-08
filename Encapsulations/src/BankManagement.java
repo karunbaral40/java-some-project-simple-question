76e3194a9261e8e78d693bd6726b371b1e48f48b
karunbaral40 <karunbaral40@gmail.com>
2025-12-08 21:14:30 +0545

    before break

---
+++ Encapsulations/src/BankManagement.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+//- Create a BankAccount class with private fields accountNumber and balance.
+//        - Add getters and setters with validation (e.g., balance cannot be negative).
+//        - Write a test program that deposits and withdraws money safely
+class BankAccount{
+    private long accountNumber;
+    private double balance;
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
+public class BankManagement {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BankAccount Bank= new BankAccount();
+        System.out.println("enter the balance");
+        Bank.setBalance(sc.nextDouble());
+        System.out.println("enter the account number");
+        Bank.setAccountNumber(sc.nextLong());
+
+
+        }
+    }
+}\
