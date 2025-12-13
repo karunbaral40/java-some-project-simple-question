442db5e791238a4d0506b8b380083e4a9eae0a0f
karunbaral40 <karunbaral40@gmail.com>
2025-12-13 19:48:22 +0545

    pascal traingle

---
+++ loops/PattrenPrinting/src/PascalTriangle.java
@@ -0,0 +1,21 @@
+public class PascalTriangle {
+    public static void main(String[] args) {
+        int rows = 6;  // change this to print more rows
+
+        for (int i = 0; i < rows; i++) {
+
+            // print spaces
+            for (int s = 0; s < rows - i; s++) {
+                System.out.print(" ");
+            }
+
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+    }
+}
