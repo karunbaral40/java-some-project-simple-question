1fc03d55e18aed7dcee37261da23a023fd2f0791
karunbaral40 <karunbaral40@gmail.com>
2025-12-13 19:44:13 +0545

    star pyrimid

---
+++ loops/PattrenPrinting/src/starPyrimid.java
@@ -0,0 +1,17 @@
+public class starPyrimid {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            // spaces
+            for (int s = 1; s <= rows - i; s++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
