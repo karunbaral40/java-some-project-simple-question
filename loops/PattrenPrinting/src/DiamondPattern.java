e148bb0310429efb823d2e95fb5249e87ad4299b
karunbaral40 <karunbaral40@gmail.com>
2025-12-13 19:54:42 +0545

    DiamondPattern

---
+++ loops/PattrenPrinting/src/DiamondPattern.java
@@ -0,0 +1,19 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        // upper part
+        for (int i = 1; i <= rows; i++) {
+            for (int s = 1; s <= rows - i; s++) System.out.print(" ");
+            for (int j = 1; j <= (2 * i - 1); j++) System.out.print("*");
+            System.out.println();
+        }
+
+        // lower part
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int s = 1; s <= rows - i; s++) System.out.print(" ");
+            for (int j = 1; j <= (2 * i - 1); j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
