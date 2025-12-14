b48f8c47840eb03154fd39ad62276b14a6c10484
karunbaral40 <karunbaral40@gmail.com>
2025-12-14 21:14:55 +0545

    comment

--- loops/PattrenPrinting/src/PascalTriangle.java
+++ loops/PattrenPrinting/src/PascalTriangle.java
@@ -1,3 +1,9 @@
+//              1
+//             1 1
+//            1 2 1
+//           1 3 3 1
+//          1 4 6 4 1
+//        1 5 10 10 5 1
 public class PascalTriangle {
     public static void main(String[] args) {
         int rows = 6;  // change this to print more rows
