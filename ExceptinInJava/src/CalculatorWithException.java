0389bd6cfe5ddfc20d19f5542eb308ce03a84594
karunbaral40 <karunbaral40@gmail.com>
2025-12-16 21:19:48 +0545

    using many classes

--- ExceptinInJava/src/CalculatorWithException.java
+++ ExceptinInJava/src/CalculatorWithException.java
@@ -1,23 +1,60 @@
 import java.util.Scanner;
-class InvalidInputException extends Exception{
-    public String toString(){
-        return" don't allow string";
+
+class InvalidInputException extends Exception {
+    public String toString() {
+        return " don't allow string";
     }
-    public String getMassage() throws Exception{
+
+    public String getMassage() throws Exception {
         return "please enter number with decimal or without decimal";
     }
 }
+    class Calculator{
+        double num1;
+        double num2;
+        String op;
+        public double getNum1() {
+
+            return num1;
+        }
+        public void setNum1(double num1) {
+            this.num1 = num1;
+        }
+
+        public double getNum2() {
+
+            return num2;
+        }
+
+        public String getOp() {
+            return op;
+        }
+
+        public void setOp(String op) {
+            this.op = op;
+        }
+
+        public void setNum2(double num2) {
+            this.num2 = num2;
+
+
+    }
+}
+
 public class CalculatorWithException {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        try {
+     Calculator C=new Calculator();
             System.out.println("Enter the first number");
-            double num1=sc.nextDouble();
+            C.setNum1(sc.nextDouble());
+            sc.nextLine();
             System.out.println("Enter the second number");
-            double num2=sc.nextDouble();
+            C.setNum2(sc.nextDouble());
+            sc.nextLine();
+        System.out.println("enter the operator");
+        C.setNum1(sc.nextInt());
+
         }
-        catch (InvalidInputException e){
-            System.out.println(e);
-        }
+
     }
-}
+
