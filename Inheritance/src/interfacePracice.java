6ebf0c72b5cc2ac5d672f5a6f09401731e2036e7
karunbaral40 <karunbaral40@gmail.com>
2025-12-18 21:19:00 +0545

    anthr prcts

---
+++ Inheritance/src/interfacePracice.java
@@ -0,0 +1,15 @@
+//- class Employee
+//- class Manager extends Employee
+//Add fields and methods to show:
+// - method overriding
+//- calling parent constructor using super
+class Employee{
+    
+}
+
+
+public class interfacePracice {
+    public static void main(String[] args) {
+
+    }
+}
