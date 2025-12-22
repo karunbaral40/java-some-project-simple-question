bc99fca5d4da47485ada9f5e275271ac2022c8ef
karunbaral40 <karunbaral40@gmail.com>
2025-12-22 20:12:20 +0545

    SearchingElemementOnArrayList

--- CostumeClass/ArrayList/src/SearchingElemementOnArrayList.java
+++ CostumeClass/ArrayList/src/SearchingElemementOnArrayList.java
@@ -7,9 +7,26 @@ public class SearchingElemementOnArrayList {
         l1.add(1,2);
         l1.add(3);
         l1.add(4);
+        l1.add(2);
         l1.add(5);
+        l1.add(9);
+        l1.add(6);
+        l1.add(8);
+        l1.add(5);
+        for (int i = 0; i < l1.size(); i++) {
+            System.out.print(l1.get(i));
+            System.out.print(", ");
+        }
 
-        System.out.println(l1.contains(3));
+// it checks the element
+        System.out.println("\n"+l1.contains(3));
+// it shows element index from front
+        System.out.println("this fro first index:");
+            System.out.println(l1.indexOf(5));
+// it shows element index from end
+        System.out.println("this fro last index:");
+            System.out.println(l1.lastIndexOf(5));
+
 
         }
 
