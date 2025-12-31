6ffbc880a0f9814098983df08808e5cda8d71f0e
karunbaral40 <karunbaral40@gmail.com>
2025-12-31 19:53:21 +0545

    multiple in heretance

--- CollageWork/src/MultipleInheritance.java
+++ CollageWork/src/MultipleInheritance.java
@@ -1,6 +1,23 @@
-
+class Shapes{
+    void area(){
+        System.out.println("this is area");
+    }
+}
+class Circle1 extends Shapes{
+void parameter(){
+        System.out.println("this is parameter");
+    }
+        }
+        class Rectangle extends Circle1{
+    void volume(){
+        System.out.println("this is volume");
+    }
+        }
 public class MultipleInheritance {
     public static void main(String[] args) {
-
+Rectangle r=new Rectangle();
+r.volume();
+r.area();
+r.parameter();
     }
 }
