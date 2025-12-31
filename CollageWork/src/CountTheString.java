26387e3e7039d160fc5d1db5a50da7d6c8561dde
karunbaral40 <karunbaral40@gmail.com>
2025-12-31 20:32:50 +0545

    count string length

---
+++ CollageWork/src/CountTheString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CountTheString {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter a string");
+
+        String s=sc.nextLine();
+        System.out.println("the length of string is "+s.length());
+    }
+}
