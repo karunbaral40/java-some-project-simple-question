76fca4e57cab83efce419e8d23c9b66b154032e4
karunbaral40 <karunbaral40@gmail.com>
2025-12-31 20:47:21 +0545

    string more operation

---
+++ CollageWork/src/LowerCaseUpperCase.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LowerCaseUpperCase {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter a string");
+            String s1=sc.nextLine();
+            System.out.println("1.lower case\n2.upper case" );
+           int choice=sc.nextInt();
+            if (choice==1){
+                System.out.println(s1.toLowerCase());
+            }else {
+                System.out.println(s1.toUpperCase());
+            }
+        }
+    }
+
+}
---
+++ CollageWork/src/StringCompareUsingEquals.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StringCompareUsingEquals {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter first string");
+        String s1=sc.nextLine();
+        System.out.println("enter second string");
+        String s2=sc.nextLine();
+        if (s1.equals(s2)){
+            System.out.println("strings are equal");
+        }else {
+            System.out.println("strings are not equal");
+        }
+    }
+}
