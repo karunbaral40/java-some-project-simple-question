cd02fe3ac581a7a7ee367965b96107ac26cf61b7
karunbaral40 <karunbaral40@gmail.com>
2026-01-01 07:35:15 +0545

    more

--- coditions/src/CountVowals.java
+++ coditions/src/CountVowals.java
@@ -2,25 +2,6 @@ import java.util.Scanner;
 
 public class CountVowals {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter an string");
-        String s=sc.nextLine();
-
-        String s1=s.toUpperCase();
-        int vlen=0;
-        int clen=0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s1.charAt(i);
-            if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-                vlen++;
-
-            }
-            else if (ch >= 'A' && ch <= 'Z') { // only count letters
-                clen++;
-            }
-
-        }
-        System.out.println("no of vowel"+vlen);
-        System.out.println("no of const"+clen);
+   ;
     }
 }
