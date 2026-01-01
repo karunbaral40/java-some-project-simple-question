f69a6d130ba5cef523fcf6c003313a3977b2d0e3
karunbaral40 <karunbaral40@gmail.com>
2026-01-01 07:28:26 +0545

    more

---
+++ CollageWork/src/MethodOverloding.java
@@ -0,0 +1,16 @@
+class over1{
+    void first(int a){
+        System.out.println("the value of a is:"+a);
+    }
+    void first(int a,int b){
+        System.out.println("the value of a is:"+a+"  " +b);
+    }
+}
+public class MethodOverloding {
+    public static void main(String[] args) {
+        over1 o=new over1();
+        o.first(10);
+        o.first(10,20);
+    }
+}
+
