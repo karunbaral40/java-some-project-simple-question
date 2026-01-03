d71077225defddd83ced1c62b1b14b4b2c289143
karunbaral40 <karunbaral40@gmail.com>
2026-01-03 21:29:49 +0545

    helo

--- CollageWork/src/SimpleIntrest.java
+++ CollageWork/src/SimpleIntrest.java
@@ -2,7 +2,7 @@ public class SimpleIntrest {
     public static void main(String[] args) {
         int p=10000;
         int t=2;
-        int r=12;
+        int r=12;//in percantage
         float SI=(p*t*r)/100;
         System.out.println("simple intrest:"+SI);
 
