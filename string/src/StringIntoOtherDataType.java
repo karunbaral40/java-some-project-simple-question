67248b926c0a11ed88784393c57ed29807bc6258
karunbaral40 <karunbaral40@gmail.com>
2026-01-04 21:50:50 +0545

    StringIntoOtherDataType

---
+++ string/src/StringIntoOtherDataType.java
@@ -0,0 +1,10 @@
+public class StringIntoOtherDataType {
+    public static void main(String[] args) {
+        String s1="12";
+        String s2="14";
+        int a =Integer.parseInt(s1);
+        int b =Integer.parseInt(s2);
+        int c=a+b;
+        System.out.println(c);
+    }
+}
