bb26f6ae7d34d7413d885f7d0ec1ebe4f904f046
karunbaral40 <karunbaral40@gmail.com>
2026-01-14 08:38:28 +0545

    Update DatabaseWithJava.java

--- DatabaseWithJava/src/DatabaseWithJava.java
+++ DatabaseWithJava/src/DatabaseWithJava.java
@@ -11,10 +11,10 @@ public class DatabaseWithJava {
             System.out.println("Connected to the database!");
 
             Statement statement=connection.createStatement();
-            ResultSet resultSet=statement.executeQuery("select * from student");
+            ResultSet resultSet=statement.executeQuery("SELECT s.RollNo,s.name AS student_name, s.age,s.contact, s.email,    t.name,c.course_name, c.course_fee FROM student s INNER JOIN course c  ON s.course_id = c.course_id INNER JOIN teacher t ON c.teacher_id = t.teacher_id;\n ");
 
             while(resultSet.next()){
-                System.out.println(" | "+resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+resultSet.getInt(3)+" | "+resultSet.getLong(4)+" | "+resultSet.getString(5)+"|") ;
+                System.out.println(" | "+resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+resultSet.getInt(3)+" | "+resultSet.getLong(4)+" | "+resultSet.getString(5)+"|"+resultSet.getString(6)+"|"+resultSet.getString(7)+"|"+resultSet.getInt(8)+"|") ;
             }
 
             connection.close();
