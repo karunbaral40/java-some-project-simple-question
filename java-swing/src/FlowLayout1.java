8e22078ede956306551cf834b9e809e0ee42b0c5
karunbaral40 <karunbaral40@gmail.com>
2026-01-23 20:00:39 +0545

    finish flow layout manager

--- java-swing/src/FlowLayout1.java
+++ java-swing/src/FlowLayout1.java
@@ -12,19 +12,24 @@ public class FlowLayout1 {
 JFrame frame=new JFrame();
 frame.setSize(500,500);
 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-frame.setLayout(new FlowLayout(FlowLayout.LEADING));
+frame.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+
+JPanel panel=new JPanel();
+panel.setPreferredSize(new Dimension(250,250));
+panel.setBackground(Color.LIGHT_GRAY);
+panel.setLayout(new FlowLayout());
 
 
-
-        frame.add(new JButton("1"));
-        frame.add(new JButton("2"));
-        frame.add(new JButton("3"));
-        frame.add(new JButton("4"));
-        frame.add(new JButton("5"));
-        frame.add(new JButton("6"));
-        frame.add(new JButton("7"));
-        frame.add(new JButton("8"));
-        frame.add(new JButton("9"));
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
