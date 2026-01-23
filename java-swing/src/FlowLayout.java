db14b829a00a0bec35800ae145cb2d0cc79ffc7e
karunbaral40 <karunbaral40@gmail.com>
2026-01-23 19:44:01 +0545

    flow layout start

---
+++ java-swing/src/FlowLayout.java
@@ -0,0 +1,9 @@
+public class FlowLayout {
+    public static void main(String[] args) {
+
+    //Flow layout :-
+     //   places component in a row sized at their preferred size if the horizontal
+     // space in the container is too small the flowlayout class uses the next available row
+
+    }
+}
