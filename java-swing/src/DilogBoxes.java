192804a9ad2ed46d81eeac447d1e9fffcb428ee6
karunbaral40 <karunbaral40@gmail.com>
2026-01-25 17:02:16 +0545

    diolughe button

--- java-swing/src/DilogBoxes.java
+++ java-swing/src/DilogBoxes.java
@@ -1,5 +1,28 @@
+import com.sun.jdi.IntegerValue;
+
+import javax.swing.*;
+
 public class DilogBoxes {
     public static void main(String[] args) {
-        
+// jOOptionPane :-
+        //      Pop up a standard dialogue box that prompts user for
+        //      value or information of something
+//        JOptionPane.showMessageDialog(null,"this is some useless info","title",JOptionPane.PLAIN_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"this is some useless info","title",JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"this is some useless info","title",JOptionPane.QUESTION_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"this is some useless info","title",JOptionPane.WARNING_MESSAGE);
+//        JOptionPane.showMessageDialog(null,"this is some useless info","title",JOptionPane.ERROR_MESSAGE);
+
+//     int ans= JOptionPane.showConfirmDialog(null,"bro do you even code","code please!!",JOptionPane.YES_NO_CANCEL_OPTION);
+//if (ans==0){
+//    JOptionPane.showMessageDialog(null,"you Liar","yes",JOptionPane.WARNING_MESSAGE);
+//} else if (ans==1) {
+//    JOptionPane.showMessageDialog(null,"You are not lying this time ","no",JOptionPane.INFORMATION_MESSAGE);
+//}else {
+//    JOptionPane.showMessageDialog(null,"How dare you to cancel my Question!!!","cancel",JOptionPane.ERROR_MESSAGE);
+//}
+        String name=JOptionPane.showInputDialog("what is your name?");
+        System.out.println("Hello!"+name);
+        JOptionPane.showOptionDialog(null,"yor are awesome","secret massage",JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE,null,null,0);
     }
 }
