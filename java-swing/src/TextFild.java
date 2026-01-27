03eabeaa675d45659f62f1b7f6ed4c9a6b1840ef
karunbaral40 <karunbaral40@gmail.com>
2026-01-27 15:29:42 +0545

    text field

--- java-swing/src/TextFild.java
+++ java-swing/src/TextFild.java
@@ -1,9 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class MyClass extends JFrame implements ActionListener {
+
+    MyClass(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        JButton button=new JButton("submit");
+
+        JTextField textField =new JTextField();
+        textField.setPreferredSize(new Dimension(250,40));
+        this.add(textField);
+        this.pack();
+        this.setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
 public class TextFild {
     public static void main(String[] args) {
         //JTextField:-
         //          A GUI textbox component that can be used to
         //          add set and get text
-
-        
+        MyClass frame=new MyClass();
     }
 }
