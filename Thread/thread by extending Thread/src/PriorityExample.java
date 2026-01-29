624f6a95f098e9bfcb321e019ded0d3267ecf25f
karunbaral40 <karunbaral40@gmail.com>
2026-01-29 21:32:12 +0545

    asdyfuhgijhb

---
+++ Thread/thread by extending Thread/src/PriorityExample.java
@@ -0,0 +1,19 @@
+class PriorityExample extends Thread {
+    public void run() {
+        System.out.println(Thread.currentThread().getName() +
+                " Priority: " + Thread.currentThread().getPriority());
+    }
+
+    public static void main(String[] args) {
+        PriorityExample t1 = new PriorityExample();
+        PriorityExample t2 = new PriorityExample();
+
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+
+        t1.start();
+        t2.start();
+    }
+}
+
+
