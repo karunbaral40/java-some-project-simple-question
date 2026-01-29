90914b7473a5409ffefd4608c905a1ab1f18830f
karunbaral40 <karunbaral40@gmail.com>
2026-01-29 21:30:48 +0545

    runnable

---
+++ Thread/thread by extending Thread/src/MyRunnable.java
@@ -0,0 +1,10 @@
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Thread running using Runnable interface");
+    }
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();
+    }
+}
