9c7f3a06e77952fc7e346570d54cdbf5a3485adb
karunbaral40 <karunbaral40@gmail.com>
2026-01-30 21:03:10 +0545

    new

--- java-swing/src/TextFild.java
+++ java-swing/src/TextFild.java
@@ -4,20 +4,25 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class MyClass extends JFrame implements ActionListener {
-
+    JButton button;
+    JTextField textField;
     MyClass(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
-        JButton button=new JButton("submit");
-
-        JTextField textField =new JTextField();
+      button=new JButton("submit");
+button.addActionListener(this);
+         textField =new JTextField();
         textField.setPreferredSize(new Dimension(250,40));
         this.add(textField);
+        this.add(button);
         this.pack();
         this.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+if (e.getSource()==button){
+    System.out.println( "Welcome"+textField.getText());
+}
 
     }
 }
