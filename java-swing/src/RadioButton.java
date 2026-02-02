4cbb5b132f4ee8dfa70e7ca49ca8ec6b74730110
karunbaral40 <karunbaral40@gmail.com>
2026-02-02 16:33:28 +0545

    radio button in java

--- java-swing/src/RadioButton.java
+++ java-swing/src/RadioButton.java
@@ -1,10 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 
-class RDButton extends JFrame {
+class
+RDButton extends JFrame {
     RDButton(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+        JRadioButton pizzaButton=new JRadioButton("pizza");
+        JRadioButton HamBurgerButton=new JRadioButton("HamBurger");
+        JRadioButton HotDogButton=new JRadioButton("HotDog");
+        ButtonGroup group=new ButtonGroup();
+        group.add(pizzaButton);
+        group.add(HamBurgerButton);
+        group.add(HotDogButton);
+this.add(pizzaButton);
+this.add(HamBurgerButton);
+this.add(HotDogButton);
         this.pack();;
         this.setVisible(true);
     }
