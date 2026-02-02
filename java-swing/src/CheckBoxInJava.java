57d0ef7d5b587cb8d50ef686038dfaef00e0e4c3
karunbaral40 <karunbaral40@gmail.com>
2026-02-02 16:27:15 +0545

    asdd

--- java-swing/src/CheckBoxInJava.java
+++ java-swing/src/CheckBoxInJava.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+class neew extends JFrame implements ActionListener {
+    JButton button;
+    JCheckBox checkBox;
+    ImageIcon XIcon;
+    ImageIcon icon;
+neew(){
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(new FlowLayout());
+    XIcon=new ImageIcon("C:\\Users\\Acer\\OneDrive\\Documents\\Desktop\\chatgptQustionForJava\\java-swing\\src\\wrong.png");
+    icon=new ImageIcon("C:\\Users\\Acer\\OneDrive\\Documents\\Desktop\\chatgptQustionForJava\\java-swing\\src\\correct.png");
+button=new JButton("submit");
+    button.addActionListener(this);
+
+checkBox=new JCheckBox();
+    checkBox.setText("I'm not a robot");
+    checkBox.setFocusable(false);
+    checkBox.setFont(new Font("Consolas",Font.PLAIN,35));
+checkBox.setIcon(XIcon);
+checkBox.setSelectedIcon(icon);
+    this.add(checkBox);
+    this.add(button);
+     this.pack();
+    this.setVisible(true);
+
+}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    if (e.getSource()==button){
+        System.out.println(checkBox.isSelected());
+    }
+    }
+}
 public class CheckBoxInJava {
+    public static void main(String[] args) {
+        //JCheckBox=A GUI component that can be selected
+        //           or deselect
+        new neew();
+    }
 }
---
+++ java-swing/src/RadioButton.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+class RDButton extends JFrame {
+    RDButton(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.pack();;
+        this.setVisible(true);
+    }
+
+}
+public class RadioButton {
+    public static void main(String[] args) {
+        new RDButton();
+        //JRadioButton :-
+        //               one or more button in a grouping in
+        // which only one may be selected per group
+
+    }
+}
