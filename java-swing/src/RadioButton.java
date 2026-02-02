cb7bed6f4713c09cbdc3c902a7536610ed7b8a7b
karunbaral40 <karunbaral40@gmail.com>
2026-02-02 16:45:23 +0545

    git push

--- java-swing/src/RadioButton.java
+++ java-swing/src/RadioButton.java
@@ -8,22 +8,27 @@ RDButton extends JFrame implements ActionListener {
     JRadioButton pizzaButton;
     JRadioButton HamBurgerButton;
     JRadioButton HotDogButton;
+    JRadioButton MOMO;
     RDButton(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
         pizzaButton=new JRadioButton("pizza");
         HamBurgerButton=new JRadioButton("HamBurger");
          HotDogButton=new JRadioButton("HotDog");
+         MOMO=new JRadioButton("MOMO");
         ButtonGroup group=new ButtonGroup();
         group.add(pizzaButton);
         group.add(HamBurgerButton);
         group.add(HotDogButton);
+        group.add(MOMO);
         pizzaButton.addActionListener(this);
         HamBurgerButton.addActionListener(this);
         HotDogButton.addActionListener(this);
+        MOMO.addActionListener(this);
 this.add(pizzaButton);
 this.add(HamBurgerButton);
 this.add(HotDogButton);
+this.add(MOMO);
         this.pack();;
         this.setVisible(true);
     }
@@ -34,6 +39,8 @@ this.add(HotDogButton);
             System.out.println("your pizza is getting ready wait 10 min-----");
         } else if (e.getSource()==HamBurgerButton) {
             System.out.println("your HamBurger  is getting ready wait 7 min-----");
+        }  else if (e.getSource()==MOMO) {
+            System.out.println("your HamBurger  is getting ready wait 20 min-----");
         }else {
             System.out.println("your HotDog  is getting ready wait 5 min-----");
         }
