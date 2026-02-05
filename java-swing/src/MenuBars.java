8bd01419bd67ee71d61e77f9c64658c3d1986220
karunbaral40 <karunbaral40@gmail.com>
2026-02-05 21:38:19 +0545

    menu bars

--- java-swing/src/MenuBars.java
+++ java-swing/src/MenuBars.java
@@ -2,26 +2,41 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 class Halku extends JFrame implements ActionListener {
+    JMenuBar menuBar;
+    JMenuItem loadItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
+    JMenu editMenu;
+    JMenu helpMenu;
     Halku(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
         this.setSize(500,500);
-        JMenuBar menuBar=new JMenuBar();
+         menuBar=new JMenuBar();
 
 
         JMenu fileMenu=new JMenu("file");
-        JMenuItem loadItem=new JMenuItem("load");
-        JMenuItem saveItem=new JMenuItem("Save");
-        JMenuItem exitItem=new JMenuItem("Exit" );
+         loadItem=new JMenuItem("load");
+         saveItem=new JMenuItem("Save");
+        exitItem=new JMenuItem("Exit" );
+        loadItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        loadItem.setMnemonic(KeyEvent.VK_L);
+        saveItem.setMnemonic(KeyEvent.VK_S);
+        exitItem.setMnemonic(KeyEvent.VK_E);
+        
         fileMenu.add(loadItem);
         fileMenu.add(saveItem);
         fileMenu.add(exitItem);
 
 
-        JMenu editMenu=new JMenu("Edit");
-        JMenu helpMenu=new JMenu("Help");
+        editMenu=new JMenu("Edit");
+        helpMenu=new JMenu("Help");
         menuBar.add(fileMenu);
         menuBar.add(editMenu);
         menuBar.add(helpMenu);
@@ -31,7 +46,15 @@ class Halku extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+if (e.getSource()==loadItem){
+    System.out.println("beep beep beep");
+}
+if (e.getSource()==saveItem){
+    System.out.println("saved suceffuly");
+}
+if(e.getSource()==exitItem){
+    System.exit(0);
+}
     }
 }
 public class MenuBars {
