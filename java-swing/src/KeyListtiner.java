627f8f0ce839206b21ed74df152e9b0b4426bafd
karunbaral40 <karunbaral40@gmail.com>
2026-02-07 16:25:41 +0545

    add this too

---
+++ java-swing/src/KeyListtiner.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+class asdf extends myframe{
+    asdf(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+
+    }
+
+}
+public class KeyListtiner {
+    public static void main(String[] args) {
+
+    }
+}
