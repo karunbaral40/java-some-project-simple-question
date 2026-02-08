962e8f0cea84684b03ac601849c1194408188608
karunbaral40 <karunbaral40@gmail.com>
2026-02-08 14:00:07 +0545

    add

---
+++ CollectionFramWork/ArrayList/src/classExample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class classExample {
+        public static void main(String[] args) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add("Java");
+            list.add("Python");
+
+            System.out.println(list);
+        }
+    }
+
