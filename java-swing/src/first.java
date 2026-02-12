0493cdbda7529085a77dee3af869def8d6395220
karunbaral40 <karunbaral40@gmail.com>
2026-02-12 20:31:16 +0545

    jdfkjak

--- java-swing/src/first.java
+++ java-swing/src/first.java
@@ -18,7 +18,11 @@ public class first {
 //       frame.getContentPane().setBackground(new Color(150,155,255));//👉making custom color rgb
 //        frame.getContentPane().setBackground(new Color(0X012345));//👉making custom color hexa
 
-      new myframe();
+      JFrame f=new JFrame();
+      f.setSize(300,300);
+      f.setVisible(true);
+
+//      new myframe();
 
   }
 
